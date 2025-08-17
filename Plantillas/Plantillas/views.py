60af1172015a9d716096aefae65aebf2fc6de42e
Raul Alejandro <rauledea86@gmail.com>
2025-08-17 10:37:52 -0400

    Aprendiendo Django

---
+++ Plantillas/Plantillas/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def simple(request):
+    
+    
+    return render(request, "simple.html",{})
+
+
+def dinamico(request,name):
+    categories =['spanish','english','chinese']
+    ctx ={
+        "name": name,
+        'categories':categories,
+    }
+    
+    return render(request, "dinamico.html",ctx)
